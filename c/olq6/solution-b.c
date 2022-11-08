62536f3dd3dfdf7f5f0eaa8ccce728ef410b4b5d
Achrip <ashraf.alif@gmail.com>
2022-11-08 16:21:25 +0700

    initial commit

---
+++ c/olq6/solution-b.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int rep; 
+    
+    scanf("%d", &rep); 
+    getchar(); 
+
+    int arr[rep], idx[rep], tmp[rep]; 
+
+    for (int i = 0; i < rep; i++) {
+        scanf("%d", &idx[i]); 
+        getchar(); 
+    }
+
+    for (int i = 0; i < rep; i++) {
+        scanf("%d", &tmp[i]); 
+        getchar(); 
+    }
+
+    for (int i = 0; i < rep; i++) {
+        arr[idx[i]] = tmp[i];
+    }
+
+    for (int i = 0; i < rep; i++) {
+        printf("%d", arr[i]); 
+        
+        if (i == rep-1) printf("\n"); 
+        else printf(" ");
+    }
+}
