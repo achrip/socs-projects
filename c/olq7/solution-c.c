843149f828133abf508e6c9e501d4398d8dde50c
achrip <ashraf.alif@gmail.com>
2022-11-16 08:20:10 +0700

    workaround needs tobe implemented

--- c/olq7/solution-c.c
+++ c/olq7/solution-c.c
@@ -1,52 +1,61 @@
 // #include <stdio.h>
 
-// int flagDuplicate(int i, int *a); 
+// long long int flagDuplicate(long long int n, long long int *arr); 
 
 // int main() {
-//     int size; 
+//     long long int size; 
 
-//     scanf("%d", &size); 
+//     scanf("%lld", &size); 
 //     getchar(); 
 
-//     int arr[size], count = size; 
+//     long long int arr[size], count = size; 
 //     for (int i = 0; i < size; i++) {
-//         scanf("%d", &arr[i]); 
+//         scanf("%lld", &arr[i]); 
 //         getchar(); 
 //     }
 //     count -= flagDuplicate(size, arr); 
-//     printf("%d\n", count);
+//     printf("%lld\n", count);
 
 //     return 0; 
 // }
 
-// int flagDuplicate(int n, int *arr) {
-//     int x = 0; 
+// long long int flagDuplicate(long long int n, long long int *arr) {
+//     int x = 0, temp[n]; 
 //     for (int i = 0; i < n - 1; i++) {
 //         for (int j = i + 1; j < n; j++) {
-//             if (arr[i] == arr[j]) x++; 
+//             if (arr[i] == arr[j]) {
+//                 for (int k = 0; k < n; k++) {
+//                     if (temp[k] == arr[j]) continue; 
+//                     else temp[k] = arr[j];
+//                 }
+//             }
 //         } 
 //     }
 
 //     return x; 
 // }
-#include <stdio.h>
-
-int main() {
-    int size; 
-
-    scanf("%d", &size); 
-    getchar(); 
-
-    long long int arr[5001] = {0}, count = 0, temp;  
-    for (int i = 0; i < size; i++) {
-        scanf("%lld", &temp); 
-        getchar();
-        if (arr[temp] == 0) {
-            count++; 
-            arr[temp]++;
-        }
+#include<stdio.h>
+int main()
+{
+    int numbers[15] = {1,1,2,2,2,3,4,5,6,6,7,7,7,8,9}, i, j;
+    int arr2[15], k = 0, k1 = 0;
+    int flag = 0;
+    for(i = 0; i < 15; i++)
+    {
+       for(j = 0; j < 15; j++)
+       {
+           flag = 0;
+           if(i != j && numbers[i] == numbers[j])
+           {
+               for(k1 = 0; k1 < k; k1++)
+                   if(arr2[k1] == numbers[j])
+                     flag = 1;
+               if(flag != 1)
+                   arr2[k++] = numbers[j];
+           }
+       }
     }
-    printf("%d\n", count);
-
-    return 0; 
+    for(i = 0; i < k; i++)
+      printf("Number %d has duplicate values\n",arr2[i]);
+    return 0;
 }
