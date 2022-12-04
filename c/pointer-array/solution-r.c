7cd0ac323344f004f3eb0a2e690b91b38c8f3922
achrip <ashraf.alif@gmail.com>
2022-12-04 17:44:30 +0700

    added+finished problem R

---
+++ c/pointer-array/solution-r.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int testCases; 
+    scanf("%d", &testCases);    
+    getchar(); 
+
+    for (int i = 0; i < testCases; i++) {
+        int changeCount; 
+        char string[1001];
+        scanf("%s", string); 
+        getchar();  
+        scanf("%d", &changeCount); 
+        getchar(); 
+
+        for (int j = 0; j < changeCount; j++) {
+            char from, to; 
+            scanf("%c %c", &from, &to);
+            getchar(); 
+
+            for (int k = 0; k < strlen(string); k++) {
+                if (string[k] == from) string[k] = to; 
+            }
+        }
+
+        printf("Case #%d: %s\n", i+1, string); 
+    }
+}
