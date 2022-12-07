1abe33cc3d3808071b4ffffc4cd58c69f7a517b5
Linux User <jottr@DESKTOP-EVD6KR6.localdomain>
2022-12-07 15:46:10 +0000

    finished all arithmetic.

--- c/arithmetic-op/problem-t/solution.c
+++ c/arithmetic-op/problem-t/solution.c
@@ -1,63 +1,12 @@
 #include <stdio.h>
 
 void main(){
-    int hr_start[10], min_start[10], hr_end[10], min_end[10]; 
-    char course_code[10][10];
+    int hr_start, min_start, hr_end, min_end; 
+    char course_code[15];
 
     for (int i=0; i<5; ++i){
-        scanf("%s %02d:%02d-%02d:%02d", course_code[i], &hr_start[i], &min_start[i], &hr_end[i], &min_end[i]); 
+        scanf("%s %d:%d-%d:%d", course_code, &hr_start, &min_start, &hr_end, &min_end); 
         getchar();
-
-        hr_start[i]--;
-        hr_end[i]--;
-        
-        switch (hr_start[i]){
-        case 6:
-            hr_start[i]=20;
-            break;
-
-        case 25: 
-            hr_start[i]=8;
-            break; 
-
-        default:
-            break;
-        }
-
-        switch (hr_end[i]){
-        case 6:
-            hr_end[i]=20;
-            break;
-
-        case 25: 
-            hr_end[i]=8;
-            break; 
-                    
-        default:
-            break;
-        }
-
-        // switch (min_start[i]){
-        // case -1:
-        // case 60:
-        //     min_start[i] = 0; 
-        //     break;
-        
-        // default:
-        //     break;
-        // }
-
-        // switch (min_end[i]) {
-        //     case -1:
-        //     case 60: 
-        //         min_end[i] = 0; 
-        //         break; 
-
-        //     default: break; 
-        // }
-    }
-
-    for (int i=0; i<5; ++i){
-        printf("%s %02d:%02d-%02d:%02d\n", course_code[i], hr_start[i], min_start[i], hr_end[i], min_end[i]);
+        printf("%s %02d:%02d-%02d:%02d\n", course_code, hr_start-1, min_start, hr_end-1, min_end);
     }
 }
